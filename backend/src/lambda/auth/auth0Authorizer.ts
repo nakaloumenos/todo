55f1150a95fc5c7a6bb1e8192a883f5921fb94dd
Nikolaos Alexandros Kaloumenos <nakaloumenos@gmail.com>
2020-04-14 19:18:36 +0100

    remove comments

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -1,18 +1,12 @@
 import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
 import 'source-map-support/register'
-
-// import { verify, decode } from 'jsonwebtoken'
 import { verify } from 'jsonwebtoken'
 import { createLogger } from '../../utils/logger'
 import Axios from 'axios'
-// import { Jwt } from '../../auth/Jwt'
 import { JwtPayload } from '../../auth/JwtPayload'
 
 const logger = createLogger('auth')
 
-// TODO: Provide a URL that can be used to download a certificate that can be used
-// to verify JWT token signature.
-// To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
 const jwksUrl = 'https://nakaloumenos.auth0.com/.well-known/jwks.json'
 
 export const handler = async (
@@ -62,10 +56,6 @@ async function verifyToken(authHeader: string): Promise<JwtPayload> {
   const jwks = res.data.keys
 
   return verify(token, jwks, { algorithms: ['RS256'] }) as JwtPayload
-
-  // TODO: Implement token verification
-  // You should implement it similarly to how it was implemented for the exercise for the lesson 5
-  // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
 }
 
 function getToken(authHeader: string): string {
